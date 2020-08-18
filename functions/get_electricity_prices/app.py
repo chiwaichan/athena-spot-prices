083de812fb4638ea37cc136c4461d06d55ece4a6
Chiwai Chan <chiwaichan@Chiwais-MacBook-Pro.local>
2020-08-18 20:01:03 +1200

    uploading files to S3

--- functions/get_electricity_prices/app.py
+++ functions/get_electricity_prices/app.py
@@ -1,10 +1,27 @@
 import requests
+import os
+import pickle
+import boto3
+import time
+import json
 
 def lambda_handler(event, context):
     
+    bucketName = os.environ['PricesElectrictyInfoS3Bucket']
+
     payload = {'chart_keys': 'prices_last_five_mins_map'}
     r = requests.get('https://www.electricityinfo.co.nz/dashboard/updates', params=payload)
 
-    print (r.text)
+    parsed_json = (json.loads(r.text))
+
+    jsonFileData = {}
+    
+    for n in parsed_json['charts']['prices_last_five_mins_map']['data']['nodes']:
+        jsonFileData[n['gip_gxp_full']] = n['price']
+        jsonFileData['date_time'] = n['trading_date'] + " " + str(n['market_time'])
+
+    s3 = boto3.resource('s3')
+    obj = s3.Object(bucketName, 'electricity-info-' + time.strftime("%Y%m%d%H%M") + '.json')
+    obj.put(Body=json.dumps(jsonFileData))
 
     return {}
